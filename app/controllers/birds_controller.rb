3aea78b721eed4bbd76335cc0d3d70c1ec93cb34
jkatem <jenmiao@gmail.com>
2020-08-19 15:15:34 -0700

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,25 @@
+
+# don't need instance variable anymore because we are no longer rendering to ERB 
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all
+    render json: birds
   end
 end
+
+# another option to do --
+    # class BirdsController < ApplicationController
+    #   def index
+    #     @birds = Bird.all
+    #     render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] } ==> rendering two keys, each pointing to an array
+    #   end
+    # end
+
+
+# ANOTHER OPTION:
+    # class BirdsController < ApplicationController
+    #   def index
+    #     @birds = Bird.all
+    #     render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }.to_json
+    #   end
+    # end
